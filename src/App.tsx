31239b85cf86d52db2f671ad9e26773acdec78ea
robis <robisimonneau@gmail.com>
2022-04-14 14:09:49 +0200

    Début du projet

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './header';
+
+import './style/App.scss'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
