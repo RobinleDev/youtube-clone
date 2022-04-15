06bb13d632c9ff861c626086e713c156403a7133
robis <robisimonneau@gmail.com>
2022-04-15 14:12:05 +0200

    Navbar terminé

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,7 +1,11 @@
 import './style/navbar.scss';
 
 import menu from './img/menu.svg';
-
+import home from './img/home.svg';
+import compass from './img/compass.svg';
+import shorts from './img/shorts.svg';
+import sub from './img/sub.svg';
+import video from './img/video-lib.svg';
 
 export default function Navbar(){
 
@@ -10,18 +14,23 @@ export default function Navbar(){
         <nav>
             <img src={menu} alt="" />
             <div className="accueil">
+                <img src={home} alt="" />
                 <span>Accueil</span>
             </div>
             <div className="explorer">
+                <img src={compass} alt="" />
                 <span>Explorer</span>
             </div>
             <div className="shorts">
+                <img src={shorts} alt="" />
                 <span>Shorts</span>
             </div>
             <div className="abonnements">
+                <img src={sub} alt="" />
                 <span>Abonnement</span>
             </div>
             <div className="biblioteque">
+                <img src={video} alt="" />
                 <span>Bibliotèque</span>
             </div>
         </nav>
